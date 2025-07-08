dba7d737cba578687287a3b0f3d2080709206e31
Jilio013 <elmo2trappy@gmail.com>
2025-07-08 04:28:18 +0100

    Create tokenizer.h

---
+++ tokenizer.h
@@ -0,0 +1,11 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <string>
+#include <vector>
+
+std::string getinput();
+
+std::vector<std::string> tokenized(const std::string& input);
+
+#endif 
